5f3b367b10b1edcc397b484dfa33e4389c603204
Jonathan Brinson <jonathanbrinsontenor@gmail.com>
2020-11-30 23:52:53 +0000

    Automatically backed up by Learn

--- cartoon_collections.rb
+++ cartoon_collections.rb
@@ -12,6 +12,6 @@ def list_dwarves(array)
   #
   # Print a numbered list of each element
   array.each_with_index do |name, num|
-    puts "#{num}. #{name}" 
+    puts "#{num + 1}. #{name}" 
   end 
 end
