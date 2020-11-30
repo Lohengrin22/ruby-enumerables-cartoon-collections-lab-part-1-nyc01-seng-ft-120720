3835fb072bc83e281cf70b27ba53d2c405b40430
Jonathan Brinson <jonathanbrinsontenor@gmail.com>
2020-11-30 23:40:44 +0000

    Automatically backed up by Learn

--- cartoon_collections.rb
+++ cartoon_collections.rb
@@ -4,7 +4,7 @@ def greet_characters(array)
   # Print a custom greeting for each element
   array = []
   array.each do |greeting|
-    puts "Hello #{greeting} 
+    puts "Hello #{greeting}!" 
   end 
 end
 
